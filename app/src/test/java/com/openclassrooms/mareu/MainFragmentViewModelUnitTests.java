92f3027b9391f68c2d3f050f165683aef9fd53ec
Arnaudv6 <Arnaudv6@users.noreply.github.com>
2021-06-14 16:59:19 +0200

    17h

---
+++ app/src/test/java/com/openclassrooms/mareu/MainFragmentViewModelUnitTests.java
@@ -0,0 +1,47 @@
+package com.openclassrooms.mareu;
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+
+import com.openclassrooms.mareu.model.Meeting;
+import com.openclassrooms.mareu.repository.MasterDetailRepository;
+import com.openclassrooms.mareu.repository.MeetingsRepository;
+import com.openclassrooms.mareu.ui.main.MainFragmentViewModel;
+import com.openclassrooms.mareu.ui.show.ShowMeetingFragmentViewModel;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainFragmentViewModelUnitTests {
+
+    private MeetingsRepository meetingsRepository;
+    private MasterDetailRepository masterDetailRepository;
+    private MainFragmentViewModel viewModel;
+
+    @Rule
+    InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
+
+    @Before
+    public void setUp() {
+        meetingsRepository = new MeetingsRepository();
+        masterDetailRepository = new MasterDetailRepository();
+        viewModel = new MainFragmentViewModel(meetingsRepository, masterDetailRepository);
+    }
+
+    @Test
+    public void filterMeetingExceptiononNullMeetings(){
+        // given
+        LocalDateTime localDateTimeFilter = LocalDateTime.of(2021,6,15,8,10,0);
+        List<Meeting> list = new ArrayList<>();
+
+        // when
+        // then
+        viewModel.fil
+    }
+
+
+}
--- app/src/test/java/com/openclassrooms/mareu/ShowMeetingFragmentViewModelUnitTests.java
+++ app/src/test/java/com/openclassrooms/mareu/ShowMeetingFragmentViewModelUnitTests.java
@@ -1,5 +1,7 @@
 package com.openclassrooms.mareu;
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+
 import com.openclassrooms.mareu.model.Meeting;
 import com.openclassrooms.mareu.model.MeetingRoom;
 import com.openclassrooms.mareu.repository.MasterDetailRepository;
@@ -9,13 +11,16 @@ import com.openclassrooms.mareu.ui.show.ShowMeetingFragmentViewModel;
 import com.openclassrooms.mareu.ui.show.ShowMeetingFragmentViewState;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.function.ThrowingRunnable;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.HashSet;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
 public class ShowMeetingFragmentViewModelUnitTests {
@@ -23,6 +28,9 @@ public class ShowMeetingFragmentViewModelUnitTests {
     private MeetingsRepository meetingsRepository;
     private MasterDetailRepository masterDetailRepository;
     private ShowMeetingFragmentViewModel viewModel;
+
+    @Rule
+    public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
 
     @Before
     public void setUp() {
@@ -39,12 +47,12 @@ public class ShowMeetingFragmentViewModelUnitTests {
         HashSet<String> participants = new HashSet<>(Arrays.asList("claire@nerdzherdz.com", "jack@lamzone.fr", "jack@lamzone.net"));
         String topic = "Daily meetup";
         LocalDateTime start = LocalDateTime.of(2021, 6, 14, 8, 30, 0);
-        LocalDateTime end =  LocalDateTime.of(2021, 6, 14, 9, 35, 0);
+        LocalDateTime end = LocalDateTime.of(2021, 6, 14, 9, 35, 0);
         MeetingRoom room = MeetingRoom.ROOM_2;
 
         String[] viewStateParticipants = {"claire@nerdzherdz.com", "jack@lamzone.fr", "jack@lamzone.net"};
 
-        Meeting meeting1 = new Meeting(id, owner, participants, topic, start ,end, room);
+        Meeting meeting1 = new Meeting(id, owner, participants, topic, start, end, room);
         meetingsRepository.createMeeting(meeting1);
 
         // when
@@ -58,10 +66,19 @@ public class ShowMeetingFragmentViewModelUnitTests {
         assertEquals(topic, viewState.getTopic());
         assertEquals("08h30", viewState.getStartText());
         assertEquals("09h35", viewState.getEndText());
+        assertEquals(MeetingRoom.ROOM_2.getName(), viewState.getRoomName());
     }
 
+    @Test
+    public void initialMeetingNotFound() throws InterruptedException {
+        // given
+        int id = 44;
 
-    Meeting meeting2 = new Meeting(2, "tedy@buymore.fr", new HashSet<>(Arrays.asList("claire@nerdzherdz.com", "jack@lamzone.fr", "jack@lamzone.net")), "Project xXx", LocalDateTime.of(2021, 6, 14, 16, 15, 0), LocalDateTime.of(2021, 6, 14, 16, 40, 0), MeetingRoom.values()[1]);
+        // when
+        masterDetailRepository.setCurrentId(id);
 
-
+        // then
+        // todo: super weird to use a lambda
+        assertThrows("Non-existent meeting, id 44", NullPointerException.class, () -> LiveDataTestUtils.getOrAwaitValue(viewModel.getShowMeetingFragmentItem()));
+    }
 }
